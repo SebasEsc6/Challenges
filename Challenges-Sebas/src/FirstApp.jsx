2e9e77c681eb8330678378b1b80a4590b9473eae
SebasEsc6 <110470320+SebasEsc6@users.noreply.github.com>
2023-03-02 16:43:05 -0500

    Este commit contiene el segundo challenge :p

--- Challenges-Sebas/src/FirstApp.jsx
+++ Challenges-Sebas/src/FirstApp.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
+import { useState } from 'react'
 
-const title = 'First App';
-const FirstApp = () => {
-  return (
+const FirstApp = ({ value }) => {
+  
+  const [counter, setCounter] = useState (value)
+  
+  const handleAdd = () => {
+    setCounter(counter + 1)
+  }
+  const handleSubstract = () => {
+    setCounter(counter - 1)
+  }
+  const handleReset = () => {
+    setCounter(value)
+  }
+  
+    return (
     <>
     <div className='DivCenter'>
-    <h1> {title} </h1>
-    <span> 10 </span> 
+    <h1> Counter </h1>
+    <span> {counter} </span>
+    <button onClick={() => handleAdd()}> +1 </button>
+    <button onClick={() => handleSubstract()}> -1 </button>
+    <button onClick={() => handleReset()}> Reset </button> 
     </div>
     </>
   )
